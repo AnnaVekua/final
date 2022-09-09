0db41ac25105ed6e112b8b30db9a91d1554c76ec
AnnaVekua <anavekua1@gmail.com>
2022-09-10 01:02:06 +0400

    responsive, burgerbar

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var submitBtn = document.getElementById("submit")
-submitBtn.addEventListener("click", function () {
+var burgerBtn = document.getElementById("burgerBtn")
+var burgerNavbar = document.querySelector('.burger-navbar')
+
+submitBtn?.addEventListener("click", function () {
     alert('are you sure?')
+})
+
+burgerBtn.addEventListener("click", function () {
+    if (burgerNavbar.classList.contains("active")) {
+        burgerNavbar.classList.remove('active')
+    } else {
+        burgerNavbar.classList.add('active')
+    }
+
 })
